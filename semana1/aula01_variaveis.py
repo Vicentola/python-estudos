6f20b47adbf75469d4eb33717848e331f696ad0a
Vicente <vicentedovalle24112005@gmail.com>
2026-06-29 13:25:16 -0300

    aula 01 - variaveis e tipos

---
+++ semana1/aula01_variaveis.py
@@ -0,0 +1,21 @@
+nome = "vicente"
+idade = 20
+altura = 1.75
+programador = True
+
+print(type(nome))
+print(type(idade))
+print(type(altura))
+print(type(programador))
+
+
+nome_atendente = "cleberson"
+anos_experiencia = 5
+nota_media = 8.3
+disponivel = False
+
+print(f"Atendente: {nome_atendente}")
+print(f"Experiência: {anos_experiencia} anos")
+print(f"Nota: {nota_media}")
+print(f"Disponível: {disponivel}")
+
